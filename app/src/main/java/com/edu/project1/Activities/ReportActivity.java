cb3ee4b41f3f829520c93ae8a1564f553d5ff1b1
datlowashere <datlowashere@gmail.com>
2022-11-11 23:25:24 +0700

    report

--- app/src/main/java/com/edu/project1/Activities/ReportActivity.java
+++ app/src/main/java/com/edu/project1/Activities/ReportActivity.java
@@ -1,10 +1,14 @@
 package com.edu.project1.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.edu.project1.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -12,5 +16,38 @@ public class ReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report_activity);
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.reportproduct);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.dashboard:
+                        startActivity(new Intent(ReportActivity.this, DasboardActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.importproduct:
+                        startActivity(new Intent(ReportActivity.this, ImportActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.exportpproduct:
+                        startActivity(new Intent(ReportActivity.this, ExportActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.reportproduct:
+
+                        return true;
+                    case R.id.logout:
+                        startActivity(new Intent(ReportActivity.this, LogoutActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+
+                }
+                return false;
+            }
+        });
+
     }
 }
