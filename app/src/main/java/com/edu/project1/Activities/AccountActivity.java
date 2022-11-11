cb5edc0b2098ff1c94df0574665ebb3ee7d710bb
datlowashere <datlowashere@gmail.com>
2022-11-11 23:49:04 +0700

    change_imformation_user

--- app/src/main/java/com/edu/project1/Activities/AccountActivity.java
+++ app/src/main/java/com/edu/project1/Activities/AccountActivity.java
@@ -12,5 +12,6 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_activity);
+
     }
 }
