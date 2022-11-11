27a74aca82cb59d48119c441bedb755b8e996495
datlowashere <datlowashere@gmail.com>
2022-11-11 23:49:22 +0700

    about_app

--- app/src/main/java/com/edu/project1/Activities/IntroActivity.java
+++ app/src/main/java/com/edu/project1/Activities/IntroActivity.java
@@ -12,5 +12,6 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_activity);
+
     }
 }
