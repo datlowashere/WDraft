a96a321a716bce29e0026de1b5f156d122b058b9
datlowashere <datlowashere@gmail.com>
2022-11-11 23:24:09 +0700

    dasboard

--- app/src/main/java/com/edu/project1/Activities/DasboardActivity.java
+++ app/src/main/java/com/edu/project1/Activities/DasboardActivity.java
@@ -1,10 +1,14 @@
 package com.edu.project1.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.edu.project1.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DasboardActivity extends AppCompatActivity {
 
@@ -12,5 +16,36 @@ public class DasboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dasboard_activity);
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.dashboard);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.dashboard:
+                        return true;
+                    case R.id.importproduct:
+                        startActivity(new Intent(DasboardActivity.this, ImportActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.exportpproduct:
+                        startActivity(new Intent(DasboardActivity.this, ExportActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.reportproduct:
+                        startActivity(new Intent(DasboardActivity.this, ReportActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.logout:
+                        startActivity(new Intent(DasboardActivity.this, LogoutActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+
+                }
+                return false;
+            }
+        });
     }
 }
