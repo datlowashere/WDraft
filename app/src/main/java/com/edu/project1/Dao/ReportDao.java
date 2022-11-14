e14b243dea8da2ecf339cf20cce26776e612dc42
datlowashere <datlowashere@gmail.com>
2022-11-14 23:49:12 +0700

    report dao

--- app/src/main/java/com/edu/project1/Dao/ReportDao.java
+++ app/src/main/java/com/edu/project1/Dao/ReportDao.java
@@ -1,4 +1,20 @@
 package com.edu.project1.Dao;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.edu.project1.DBHelper.DBHelper;
+import com.edu.project1.Models.InventoryItems;
+
+import java.util.List;
+
 public class ReportDao {
+    private SQLiteDatabase db;
+    private List<InventoryItems> list;
+
+    public ReportDao(Context context) {
+        DBHelper dbHelper=new DBHelper(context);
+        db=dbHelper.getWritableDatabase();
+    }
+
 }
