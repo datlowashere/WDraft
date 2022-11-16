8dd0411ecdfdbf9563fbf464ff8f3fdcdb70383f
datlowashere <datlowashere@gmail.com>
2022-11-16 22:46:16 +0700

    account fragment

---
+++ app/src/main/java/com/edu/project1/Fragments/AccountFragment.java
@@ -0,0 +1,53 @@
+package com.edu.project1.Fragments;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edu.project1.Activities.LoginActivity;
+import com.edu.project1.R;
+
+
+public class AccountFragment extends Fragment {
+
+
+
+    public AccountFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static AccountFragment newInstance() {
+        AccountFragment fragment = new AccountFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_account, container, false);
+        view.findViewById(R.id.btnLogout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+            }
+        });
+        return view;
+    }
+}
